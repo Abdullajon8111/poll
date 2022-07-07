55f9fb152397a8a566cb6363ba2e1cae7a20299b
ssabdukarimov <ss.abdukarimov@gmail.com>
2022-07-07 20:29:05 +0500

    proba

---
+++ app/Models/Organization.php
@@ -0,0 +1,71 @@
+<?php
+
+namespace App\Models;
+
+use Backpack\CRUD\app\Models\Traits\CrudTrait;
+use Eloquent;
+use Illuminate\Database\Eloquent\Builder;
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Foundation\Auth\User as Authenticatable;
+use Illuminate\Support\Carbon;
+
+/**
+ * App\Models\Organization
+ *
+ * @property int $id
+ * @property string|null $ktut
+ * @property string|null $stir
+ * @property string|null $name
+ * @property string|null $region
+ * @property string|null $district
+ * @property string|null $address
+ * @property string|null $phone
+ * @property string|null $email
+ * @property Carbon|null $created_at
+ * @property Carbon|null $updated_at
+ * @method static Builder|Organization newModelQuery()
+ * @method static Builder|Organization newQuery()
+ * @method static Builder|Organization query()
+ * @method static Builder|Organization whereAddress($value)
+ * @method static Builder|Organization whereCreatedAt($value)
+ * @method static Builder|Organization whereDistrict($value)
+ * @method static Builder|Organization whereEmail($value)
+ * @method static Builder|Organization whereId($value)
+ * @method static Builder|Organization whereKtut($value)
+ * @method static Builder|Organization whereName($value)
+ * @method static Builder|Organization wherePhone($value)
+ * @method static Builder|Organization whereRegion($value)
+ * @method static Builder|Organization whereStir($value)
+ * @method static Builder|Organization whereUpdatedAt($value)
+ * @mixin Eloquent
+ * @method static Builder|Organization getByKtutAndStir($stir, $ktut)
+ * @property-read \Illuminate\Database\Eloquent\Collection|\App\Models\Entry[] $entries
+ * @property-read int|null $entries_count
+ * @property int|null $org_category_id
+ * @property-read \App\Models\OrgCategory|null $category
+ * @property-read \Illuminate\Database\Eloquent\Collection|\App\Models\University[] $universities
+ * @property-read int|null $universities_count
+ * @method static Builder|Organization whereOrgCategoryId($value)
+ */
+class Organization extends Authenticatable
+{
+    use CrudTrait;
+    use HasFactory;
+
+    protected $guarded = ['id'];
+
+    public function entries()
+    {
+        return $this->hasMany(Entry::class, 'participant_id');
+    }
+
+    public function category()
+    {
+        return $this->belongsTo(OrgCategory::class, 'org_category_id');
+    }
+
+    public function universities()
+    {
+        return $this->belongsToMany(University::class, 'org_univer_pivot');
+    }
+}
---
+++ app/Models/University.php
@@ -0,0 +1,90 @@
+<?php
+
+namespace App\Models;
+
+use Backpack\CRUD\app\Models\Traits\CrudTrait;
+use Eloquent;
+use Illuminate\Database\Eloquent\Builder;
+use Illuminate\Database\Eloquent\Collection;
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+use Illuminate\Support\Carbon;
+use Str;
+
+/**
+ * App\Models\University
+ *
+ * @property int $id
+ * @property string $name
+ * @property Carbon|null $created_at
+ * @property Carbon|null $updated_at
+ * @method static Builder|University newModelQuery()
+ * @method static Builder|University newQuery()
+ * @method static Builder|University query()
+ * @method static Builder|University whereCreatedAt($value)
+ * @method static Builder|University whereId($value)
+ * @method static Builder|University whereName($value)
+ * @method static Builder|University whereUpdatedAt($value)
+ * @mixin Eloquent
+ * @property string|null $slug
+ * @property int $enabled
+ * @property-read Collection|AdminUserUniversity[] $admins
+ * @property-read int|null $admins_count
+ * @method static Builder|University whereEnabled($value)
+ * @method static Builder|University whereSlug($value)
+ * @property-read Collection|\App\Models\Entry[] $entries
+ * @property-read int|null $entries_count
+ * @property int|null $univer_category_id
+ * @property-read \App\Models\UniverCategory|null $category
+ * @property-read Collection|\App\Models\Organization[] $organizations
+ * @property-read int|null $organizations_count
+ * @method static Builder|University whereUniverCategoryId($value)
+ */
+class University extends Model
+{
+    use CrudTrait;
+    use HasFactory;
+
+    protected $guarded = ['id'];
+
+    protected static function boot()
+    {
+        parent::boot();
+
+        self::creating(function (University $model) {
+            $slug = $model->slug;
+            if (!Str::length($slug))
+                $slug = Str::random();
+
+            $model->slug = Str::kebab($slug);
+        });
+
+        self::updating(function (University $model) {
+            $slug = $model->slug;
+            if (!Str::length($slug))
+                $slug = Str::random();
+
+            $model->slug = Str::kebab($slug);
+        });
+    }
+
+    public function admins()
+    {
+        return $this->belongsToMany(AdminUser::class, 'admin_user_university');
+    }
+
+    public function entries()
+    {
+        return $this->hasMany(Entry::class);
+    }
+
+    public function category()
+    {
+        return $this->belongsTo(UniverCategory::class, 'univer_category_id');
+    }
+
+    public function organizations()
+    {
+        return $this->belongsToMany(Organization::class, 'org_univer_pivot');
+    }
+}
