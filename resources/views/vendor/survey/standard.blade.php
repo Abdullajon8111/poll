29d950c3bf5e74f994b618d7c01c9d3a47dfea1c
Farhod Yusupov <61786968+farhodyusupov@users.noreply.github.com>
2022-07-29 15:52:41 +0500

    fix: TARJIMALAR

--- resources/views/vendor/survey/standard.blade.php
+++ resources/views/vendor/survey/standard.blade.php
@@ -3,20 +3,20 @@
         <h2 class="mb-0">{{ $survey->name }} </h2>
         <h6 class="font-weight-bold text-danger">{{ $university->name }}</h6>
         @if(!$eligible)
-            We only accept
-            <strong>{{ $survey->limitPerParticipant() }} {{ \Str::plural('entry', $survey->limitPerParticipant()) }}</strong>
-            per participant.
+            {{ __('We only accept') }}
+            <strong>{{ $survey->limitPerParticipant() }} {{ __('entry') }}</strong>
+            {{ __('per participant') }}.
         @endif
 
         @if($lastEntry)
-            You last submitted your answers <strong>{{ $lastEntry->created_at->diffForHumans() }}</strong>.
+            {{ __('You last submitted your answers') }} <strong>{{ $lastEntry->created_at->diffForHumans() }}</strong>.
         @endif
     </div>
 </div>
 
 @if(!$survey->acceptsGuestEntries() && auth()->guest())
     <div class="p-5">
-        Please login to join this survey.
+        {{ __('Please login to join this survey') }}.
     </div>
 @else
     @foreach($survey->sections as $section)
@@ -32,7 +32,7 @@
     @endforeach
 
     @if($eligible)
-        <button class="btn btn-dark btn-block mt-3">Submit</button>
+        <button class="btn btn-dark btn-block mt-3">{{ __('Submit') }}</button>
     @endif
 @endif
 
